2cfea87034babe86f2211153d8afc7cba2c9b971
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-24 03:20:29 +0545

    chore: cleanup the unused imports

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
@@ -3,7 +3,6 @@ package com.safespend.cashsentry.data.local_data_source.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.Date
 
 @Entity(tableName = "user_profile")
 data class UserProfile(
