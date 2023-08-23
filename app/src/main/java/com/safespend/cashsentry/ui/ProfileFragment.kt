3a0cf6c46561237c3571973564dbfc386daacc2f
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-24 03:24:46 +0545

    feat: added ui-frontend code for support of charts, settings, profile components and toggle switches

--- app/src/main/java/com/safespend/cashsentry/ui/ProfileFragment.kt
+++ app/src/main/java/com/safespend/cashsentry/ui/ProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.safespend.cashsentry.ui
 
 import android.app.Application
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.safespend.cashsentry.databinding.FragmentProfileBinding
 import com.safespend.cashsentry.viewmodel.profile.ProfileViewModel
 import kotlinx.coroutines.launch
@@ -17,6 +24,7 @@ class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var pieChart: PieChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +37,7 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         profileViewModel = ViewModelProvider.AndroidViewModelFactory(application = Application()).create(
             ProfileViewModel::class.java)
+        pieChart = binding.pieChart
 
         viewLifecycleOwner.lifecycleScope.launch {
             profileViewModel.adminProfile.collect{state ->
@@ -37,13 +46,38 @@ class ProfileFragment : Fragment() {
                 }else if(state.error.isNotEmpty()){
                     Log.i("UI error", "lol")
                 }else if(state.userProfile != null){
-                    binding.etUserEmail.text = state.userProfile.email
+                    binding.
+                    etUserEmail.text = state.userProfile.email
                     binding.etUserName.text = state.userProfile.name
                 }
             }
         }
-
+        showPieChartResult()
         return binding.root
+    }
+
+    private fun showPieChartResult(){
+        val list:ArrayList<PieEntry> = ArrayList()
+
+        list.add(PieEntry(30f,"Withdrawl"))
+        list.add(PieEntry(70f,"Deposit"))
+
+        val pieDataSet= PieDataSet(list,"")
+
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS,255)
+        pieDataSet.valueTextColor= Color.GREEN
+        pieDataSet.valueTextSize=20f
+
+        val pieData= PieData(pieDataSet)
+        pieData.setValueTextColor(Color.WHITE)
+
+        pieChart.data= pieData
+        pieChart.description.text = ""
+        pieChart.centerText = ""
+        pieChart.setHoleColor(Color.TRANSPARENT)
+        pieChart.setEntryLabelColor(Color.WHITE)
+        pieChart.animateY(2000)
+
     }
 
 }
