eeb87e10cdd03e4362c04b9c37fb4585f1a6b7bf
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-24 03:16:19 +0545

    feat: added constants

---
+++ app/src/main/java/com/safespend/cashsentry/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.safespend.cashsentry.util
+
+object Constants {
+
+    val WITHDRAWL = "WithDrawl"
+    val DEPOSIT = "Deposit"
+    val CHANNEL_ID = "com.safespend.cashsentry.ui.channel"
+
+}
