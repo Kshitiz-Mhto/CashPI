fc465481bd43572c30b9562c9ef69ae40810824c
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-23 18:19:05 +0545

    feat: added code to handle the data into UI from viewmodel

--- app/src/main/java/com/safespend/cashsentry/ui/ProfileFragment.kt
+++ app/src/main/java/com/safespend/cashsentry/ui/ProfileFragment.kt
@@ -1,17 +1,22 @@
 package com.safespend.cashsentry.ui
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.safespend.cashsentry.data.local_data_source.database.CashSentryDB
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.safespend.cashsentry.databinding.FragmentProfileBinding
+import com.safespend.cashsentry.viewmodel.profile.ProfileViewModel
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
-    private lateinit var database: CashSentryDB
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +27,21 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-        database = CashSentryDB.getDB(requireContext())
+        profileViewModel = ViewModelProvider.AndroidViewModelFactory(application = Application()).create(
+            ProfileViewModel::class.java)
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            profileViewModel.adminProfile.collect{state ->
+                if (state.isLoading) {
+                    Log.i("UI load", "lol")
+                }else if(state.error.isNotEmpty()){
+                    Log.i("UI error", "lol")
+                }else if(state.userProfile != null){
+                    binding.etUserEmail.text = state.userProfile.email
+                    binding.etUserName.text = state.userProfile.name
+                }
+            }
+        }
 
         return binding.root
     }
