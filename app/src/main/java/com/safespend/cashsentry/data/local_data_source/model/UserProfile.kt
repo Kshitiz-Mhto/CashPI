890d3b85a5e553cac8cbdc721a723510c0878264
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-23 18:16:33 +0545

    refactor: added coloum info

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
@@ -1,5 +1,6 @@
 package com.safespend.cashsentry.data.local_data_source.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Date
@@ -7,6 +8,8 @@ import java.util.Date
 @Entity(tableName = "user_profile")
 data class UserProfile(
     @PrimaryKey
+    @ColumnInfo("email")
     val email: String,
+    @ColumnInfo("name")
     val name: String
 )
