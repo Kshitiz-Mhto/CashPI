6482fd0ae4ed5b8d717403c404969a116e4daaf9
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-23 18:17:19 +0545

    fix: getAdmin method is fixed for an error

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/repository/userprofile/UserRepository.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/repository/userprofile/UserRepository.kt
@@ -2,6 +2,7 @@ package com.safespend.cashsentry.data.local_data_source.repository.userprofile
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Query
 import androidx.room.Upsert
 import com.safespend.cashsentry.data.local_data_source.model.UserProfile
 
@@ -13,5 +14,8 @@ interface UserRepository {
 
     @Delete
     suspend fun deleteUser(userProfile: UserProfile)
+
+    @Query("SELECT * FROM user_profile WHERE email = :adminEmail")
+    suspend fun getAdmin(adminEmail: String): UserProfile
 
 }
