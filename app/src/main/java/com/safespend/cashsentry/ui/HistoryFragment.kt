571d094c32dcdb88a2b3f7b76031718109daba3d
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-25 23:52:53 +0545

    feat: added history fragment

---
+++ app/src/main/java/com/safespend/cashsentry/ui/HistoryFragment.kt
@@ -0,0 +1,102 @@
+package com.safespend.cashsentry.ui
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.safespend.cashsentry.adaptors.history.HistoryRecylerViewAdapter
+import com.safespend.cashsentry.databinding.FragmentHistoryBinding
+import com.safespend.cashsentry.util.Constants
+import com.safespend.cashsentry.viewmodel.history.HistoryViewModel
+import com.safespend.cashsentry.viewmodel.history.HistoryViewModelFactory
+import kotlinx.coroutines.launch
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+    private val channelId = Constants.CHANNEL_ID
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var historyViewModel: HistoryViewModel
+    private lateinit var historyRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelId, "NotifyChannel", "This is a Notify channel.")
+
+        historyViewModel = ViewModelProvider(requireActivity(), HistoryViewModelFactory(application = requireContext())).get(HistoryViewModel::class.java)
+        historyRecyclerView = binding.historyRecyclerView
+        historyRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            historyViewModel.userHistory.collect{state ->
+                val userHistory = state.userHistory
+                if (state.isLoading) {
+                    Log.i("UI load", "lol")
+                }else if(state.error.isNotEmpty()) {
+                    Log.i("UI error", "lol")
+                }else if(state.userHistory != null){
+                    historyRecyclerView.adapter = HistoryRecylerViewAdapter(userHistory, requireContext())
+                    for (history in userHistory!!) {
+                        val notificationBuilder = NotificationCompat.Builder(requireContext(), channelId)
+                            .setSmallIcon(android.R.drawable.ic_dialog_info)
+                            .setAutoCancel(true)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        val notifyId = history.id
+                        if (history.isDesposit) {
+                            notificationBuilder
+                                    .setContentTitle("Deposit Alert!!")
+                                    .setContentText("You have successfully deposited in your Card-I of amount ${history.amt}")
+
+                        }
+                        if (history.isWithdrawl) {
+                            notificationBuilder
+                                    .setContentTitle("WithDrawl Alert!!")
+                                    .setContentText("You have succesfully withdrawl from your Card-I of amount ${history.amt}")
+
+                        }
+                        val notification = notificationBuilder.build()
+                        notificationManager.notify(notifyId, notification)
+                    }
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun createNotificationChannel(id: String, name: String, description: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                setDescription(description)
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                vibrationPattern = longArrayOf(100, 200, 300, 400, 500)
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
