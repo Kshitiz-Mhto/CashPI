16363c01f162baa5ec275056979cbca917ecb729
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-25 23:47:17 +0545

    feat: added repository reference and respective tables

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/database/CashSentryDB.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/database/CashSentryDB.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.safespend.cashsentry.data.local_data_source.model.HistoryModel
+import com.safespend.cashsentry.data.local_data_source.model.MoneyCardModel
 import com.safespend.cashsentry.data.local_data_source.model.UserProfile
+import com.safespend.cashsentry.data.local_data_source.repository.history.HistoryRepostory
+import com.safespend.cashsentry.data.local_data_source.repository.moneycard.MoneycardRepository
 import com.safespend.cashsentry.data.local_data_source.repository.userprofile.UserRepository
 
-@Database(entities = [UserProfile::class], version = 1)
+@Database(entities = [UserProfile::class, HistoryModel::class, MoneyCardModel::class], version = 1)
 abstract class CashSentryDB: RoomDatabase() {
 
     abstract fun userRepository(): UserRepository
+    abstract fun historyRepository(): HistoryRepostory
+    abstract fun moneycardRepository(): MoneycardRepository
 
     companion object {
         @Volatile
