c5515984b9a70659b1376e873a3be5b6094610b9
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-26 14:25:07 +0545

    feat: added code to handle the editing of profile dynamically to get stored in db and fetch from it

--- app/src/main/java/com/safespend/cashsentry/viewmodel/profile/ProfileViewModel.kt
+++ app/src/main/java/com/safespend/cashsentry/viewmodel/profile/ProfileViewModel.kt
@@ -2,12 +2,16 @@ package com.safespend.cashsentry.viewmodel.profile
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.safespend.cashsentry.data.local_data_source.database.CashSentryDB
 import com.safespend.cashsentry.data.local_data_source.model.UserProfile
 import com.safespend.cashsentry.util.Constants
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,21 +19,35 @@ import kotlinx.coroutines.launch
 class ProfileViewModel(application: Context): ViewModel() {
 
     var database: CashSentryDB = CashSentryDB.getDB(application)
-    private lateinit var admin: UserProfile
-
-    init {
-        getAdmin()
-    }
 
     private val _userProfile = MutableStateFlow(UserProfileState())
     var adminProfile: StateFlow<UserProfileState> = _userProfile
 
+    private val _upsertSuccessEvent = MutableLiveData<Boolean>()
+    var upsertSuccessEvent: LiveData<Boolean> = _upsertSuccessEvent
+
+    private val _deleteSuccessEvent = MutableLiveData<Boolean>(false)
+    var deleteSuccessEvent: LiveData<Boolean> = _deleteSuccessEvent
+
     fun getAdmin(){
         viewModelScope.launch (Dispatchers.IO){
-            admin = database.userRepository().getAdmin(Constants.ADMIN_EMAIL)
-            Log.i("data1", admin.email)
+            val admin: UserProfile = database.userRepository().getAdmin(Constants.ADMIN_EMAIL)
             _userProfile.value= UserProfileState(userProfile = admin)
-            Log.i("data", _userProfile.value.userProfile!!.email)
+        }
+    }
+
+    fun deletePreviousAdmin(previousEmail: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            database.userRepository().delupdateUser(previousEmail)
+            _deleteSuccessEvent.postValue(true)
+        }
+    }
+
+    fun upsertAdmin(adminProfile: UserProfile){
+        Constants.ADMIN_EMAIL = adminProfile.email
+        viewModelScope.launch(Dispatchers.IO){
+            database.userRepository().upsertUser(adminProfile)
+            _upsertSuccessEvent.postValue(true)
         }
     }
 }
--- app/src/main/java/com/safespend/cashsentry/viewmodel/profile/UserProfileState.kt
+++ app/src/main/java/com/safespend/cashsentry/viewmodel/profile/UserProfileState.kt
@@ -1,7 +1,6 @@
 package com.safespend.cashsentry.viewmodel.profile
 
 import com.safespend.cashsentry.data.local_data_source.model.UserProfile
-import kotlinx.coroutines.flow.StateFlow
 
 data class UserProfileState(
     val isLoading: Boolean = false,
