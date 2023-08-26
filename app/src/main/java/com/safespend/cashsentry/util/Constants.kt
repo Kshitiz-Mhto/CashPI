ba73d27debd2b6bf89ff6f9cfcf5e2d117cd869d
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-26 14:22:08 +0545

    fix: made constant reassignable and update floating botton logo in home fragment

--- app/src/main/java/com/safespend/cashsentry/util/Constants.kt
+++ app/src/main/java/com/safespend/cashsentry/util/Constants.kt
@@ -3,6 +3,6 @@ package com.safespend.cashsentry.util
 object Constants {
 
     val CHANNEL_ID = "com.safespend.cashsentry.ui.channel"
-    val ADMIN_EMAIL = "kalu@gmail.com"
+    var ADMIN_EMAIL = ""
 
 }
