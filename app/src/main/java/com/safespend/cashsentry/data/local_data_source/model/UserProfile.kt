79ce767c54f473b757313791e14e71d37b51f075
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-08-26 14:21:24 +0545

    fix: fixed return type error and corresponding reposiotry method implementation

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/UserProfile.kt
@@ -3,10 +3,12 @@ package com.safespend.cashsentry.data.local_data_source.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "user_profile")
 data class UserProfile(
-    @PrimaryKey
+    @PrimaryKey()
+    @NotNull
     @ColumnInfo("email")
     val email: String,
     @ColumnInfo("name")
--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/repository/userprofile/UserRepository.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/repository/userprofile/UserRepository.kt
@@ -1,10 +1,11 @@
 package com.safespend.cashsentry.data.local_data_source.repository.userprofile
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import com.safespend.cashsentry.data.local_data_source.model.UserProfile
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserRepository {
@@ -12,10 +13,14 @@ interface UserRepository {
     @Upsert
     suspend fun upsertUser(userProfile: UserProfile)
 
-    @Delete
-    suspend fun deleteUser(userProfile: UserProfile)
 
+    @Transaction
     @Query("SELECT * FROM user_profile WHERE email = :adminEmail")
     suspend fun getAdmin(adminEmail: String): UserProfile
+
+
+    @Transaction
+    @Query("DELETE FROM user_profile where email = :email")
+    suspend fun delupdateUser(email: String)
 
 }
