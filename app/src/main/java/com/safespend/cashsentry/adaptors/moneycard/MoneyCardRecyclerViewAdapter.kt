a274219a9f0dcfb67a2f2f82c711308b74aa5128
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-09-05 23:12:44 +0545

    fix: updated the primary key for moneycard model and added itemClickListener to access the element of the recyclerview

--- app/src/main/java/com/safespend/cashsentry/adaptors/moneycard/MoneyCardRecyclerViewAdapter.kt
+++ app/src/main/java/com/safespend/cashsentry/adaptors/moneycard/MoneyCardRecyclerViewAdapter.kt
@@ -7,11 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.safespend.cashsentry.R
 import com.safespend.cashsentry.data.local_data_source.model.MoneyCardModel
 
-class MoneyCardRecyclerViewAdapter(val moneycardList: List<MoneyCardModel>?, val context: Context) : RecyclerView.Adapter<MyViewHolderForMoneyCard>() {
+class MoneyCardRecyclerViewAdapter(val moneycardList: List<MoneyCardModel>?, val context: Context, val itemClickListener: OnItemClickListener) : RecyclerView.Adapter<MyViewHolderForMoneyCard>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolderForMoneyCard {
         val layoutInflator = LayoutInflater.from(parent.context)
@@ -29,26 +30,35 @@ class MoneyCardRecyclerViewAdapter(val moneycardList: List<MoneyCardModel>?, val
         when(index_element.name){
             "Master Card" -> {
                 holder.etCardName.text = "Master Card"
-                holder.etCardAmt.text = "$ " + index_element.totalAmt.toString()
+                holder.etCardAmt.text = "$ " + index_element.totalAmt
                 holder.etCardSerialNum.text = index_element.serialNum
             }
             "Visa" -> {
                 holder.etCardName.text = "Visa"
-                holder.etCardAmt.text = "$ " + index_element.totalAmt.toString()
+                holder.etCardAmt.text = "$ " + index_element.totalAmt
                 holder.etCardSerialNum.text = index_element.serialNum
             }
             "Amex" ->{
                 holder.etCardName.text = "Amex"
-                holder.etCardAmt.text = "$ " + index_element.totalAmt.toString()
+                holder.etCardAmt.text = "$ " + index_element.totalAmt
                 holder.etCardSerialNum.text = index_element.serialNum
             }
             else -> {}
         }
+        holder.cardView.setOnClickListener {
+            itemClickListener.onItemClick(position)
+        }
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
 }
 
 class MyViewHolderForMoneyCard(val view: View): RecyclerView.ViewHolder(view){
     val etCardName = view.findViewById<TextView>(R.id.etCardName)
     val etCardAmt = view.findViewById<TextView>(R.id.etCardAmount)
     val etCardSerialNum = view.findViewById<TextView>(R.id.etCardSerialNumber)
+    val cardView = view.findViewById<CardView>(R.id.statsLayoutCardview)
 }
