4c552b465f894c18904e0f1685adf95dc1e7bf44
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-09-05 23:11:57 +0545

    fix: updated the primary key for moneycard model and added itemClickListener to access the element of the recyclerview

--- app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/MoneyCardModel.kt
+++ app/src/main/java/com/safespend/cashsentry/data/local_data_source/model/MoneyCardModel.kt
@@ -3,15 +3,12 @@ package com.safespend.cashsentry.data.local_data_source.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlin.random.Random
 
 @Entity(tableName = "moneycard")
 data class MoneyCardModel(
-    @PrimaryKey(autoGenerate = false)
     val name: String,
     val totalAmt: String,
     @ColumnInfo("email") val email: String,
-    val serialNum: String = "****    ****    ****    " + generateRandomDigits().toString()
+    @PrimaryKey(autoGenerate = false)
+    val serialNum: String = ""
 )
-
-fun generateRandomDigits() =  Random.nextInt(1000, 10000)
