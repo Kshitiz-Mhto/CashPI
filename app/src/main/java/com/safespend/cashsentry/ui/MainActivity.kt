54390b7b00aa00bc21e2587e011072a38553652f
Kshitiz Mhto <kshitizmhto101@gmail.com>
2024-04-18 11:09:48 +0545

    feat: optimized the navHost controller

--- app/src/main/java/com/safespend/cashsentry/ui/MainActivity.kt
+++ app/src/main/java/com/safespend/cashsentry/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.safespend.cashsentry.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.safespend.cashsentry.R
@@ -10,12 +11,16 @@ import com.safespend.cashsentry.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_CashSentry)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
 
         // --------------------> Botton Navigation <------------------------
         binding.meowBottonNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_wallet))
@@ -36,20 +41,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun navigateToProfileFragment() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
         navController.navigate(R.id.profileFragment)
     }
 
     private fun navigateToHomeFragment() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
         navController.navigate(R.id.homeFragment)
     }
 
     private fun navigateToNotificationFragment() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
         navController.navigate(R.id.notificationFragment)
     }
 
